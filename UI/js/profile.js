45bf5b0e6ccf4ea799e619ffd49506899b7353a7
Obinna Chimezie <obinna932@gmail.com>
2018-11-21 19:20:33 +0100

    Made some Modifications

--- UI/js/profile.js
+++ UI/js/profile.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
 
-var records,investigation, rejected;
+var records,investigation, rejected, resolved;
 
 
 records = $(".orders");
 investigation = $(".investigation");
 rejected = $(".rejected");
+resolved = $(".resolved");
 
 
 
 
 records.click(function(){
+$(".resolved_interventions").hide();
 $(".interventions").hide();
 $(".rejected_interventions").hide();
 $(".under_investigation").hide();
@@ -22,15 +24,24 @@ $(".interventions").fadeIn();
 
 
 investigation.click(function(){
+    $(".resolved_interventions").hide();
     $(".interventions").hide();
     $(".rejected_interventions").hide();
     $(".under_investigation").fadeIn();
 })
 
 rejected.click(function(){
+    $(".resolved_interventions").hide();
     $(".interventions").hide();
     $(".under_investigation").hide();
     $(".rejected_interventions").fadeIn(); 
+})
+
+resolved.click(function(){
+    $(".rejected_interventions").hide();
+    $(".interventions").hide();
+    $(".under_investigation").hide();
+    $(".resolved_interventions").fadeIn();
 })
 
 })
