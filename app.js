2c7250e426600536ca8c8d3cf10c36dfede75ec2
Obinna Chimezie <obinna932@gmail.com>
2018-11-27 20:57:16 +0100

    added front end pages

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const usersRoutes = require('./api/v1/routes/users');
+const redFlagsRoutes = require('./api/v1/routes/red-flags');
+
+
+app.use(express.static(`${__dirname  }/UI`));
+
+
+app.get('*', (req, res) => {
+   res.sendFile(__dirname + '/UI/index.html');
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/v1/users', usersRoutes);
+app.use('/v1/red-flags', redFlagsRoutes);
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
